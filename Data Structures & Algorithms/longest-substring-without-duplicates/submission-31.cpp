36897534ca9052be712045ea59bcbd69c652b77e
Gazuma <60835576+Gazuma@users.noreply.github.com>
2026-07-28 22:12:02 +0530

    Add: longest-substring-without-duplicates - submission-31

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-31.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int l = 0;
+        int r = 0;
+        int result = 0;
+        int c = 0;
+        map<char,int> mp;
+        while(l<n and r<n and l<=r){
+            c = 0;
+            while(!mp[s[r]] and r<n){
+                mp[s[r]]++;
+                result = max(r-l+1,result);
+                r++;
+            }
+            
+            while(mp[s[r]] and r<n and l<n and l<=r){
+                mp[s[l]]--;
+                l++;
+            }
+            
+            if(r==n){
+                r--;
+            }
+            
+            mp[s[r]]++;
+            cout<<l<<" "<<r<<endl;
+
+            
+                        r++;
+        }
+        return result;
+    }
+};
